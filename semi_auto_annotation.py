3ee223816c99105f48a5ba76c2143a81d49ab46e
Sam Kelly <gotosam1212@gmail.com>
2020-09-25 05:29:01 +1000

    added ability to read CSV files in a (hopefully) extensible way

--- semi_auto_annotation.py
+++ semi_auto_annotation.py
@@ -29,6 +29,252 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import glob
 import numpy as np
+from abc import ABC, abstractmethod
+import pdb
+
+def np_array_to_csv_string(d):
+	def make_string(d, start, stop):
+		pts = []
+		for i in range(start, stop):
+			pts.append("%03d,%03d"%(d[i,0],d[i,1]))
+		return ','.join(pts)
+
+	antenna = make_string(d, 15, 16)
+	stripe = make_string(d, 16, 19)
+	front_leg = make_string(d, 0, 5)
+	mid_leg = make_string(d, 5, 10)
+	rear_leg = make_string(d, 10, 15)
+
+	full_string = ','.join([antenna, stripe, front_leg, mid_leg, rear_leg])
+	return full_string
+
+class Dataloader(ABC):
+	data = []
+	f_in = ""
+	camera = 0
+	frame = 0
+
+	@abstractmethod
+	def __init__(self, f_in, f_out):
+		pass
+	@abstractmethod
+	def get_image(self):
+		pass
+	@abstractmethod
+	def get_data(self):
+		pass
+	@abstractmethod
+	def next_frame(self):
+		pass
+	@abstractmethod
+	def prev_frame(self):
+		pass
+	@abstractmethod
+	def write(self, string):
+		"""	Writes the current frame's data to self.f_out
+		"""
+		pass
+	@abstractmethod
+	def set_frame(self, frame):
+		"""	Sets the frame to 'frame'
+			returns True if successful, false otherwise
+		"""
+		pass
+	@abstractmethod
+	def set_camera(self, camera):
+		"""	Sets the camera to 'camera'
+			returns True if successful, false otherwise
+		"""
+		pass
+
+class DF3DFolderDataloader(Dataloader):
+	"""	Loads the frames from a video analysed using df3d
+	"""
+
+	def __init__(self, folder, f_out):
+		self.f_in = folder
+		self.f_out = f_out
+		self.camera = 0
+		self.frame = 0
+
+		self.preds = np.load(glob.glob(os.path.join(self.f_in, "df3d", "preds*"))[0], allow_pickle=True)
+		self.preds[:,:,:,0] *= 960
+		self.preds[:,:,:,1] *= 480
+		self.pose_corr = np.load(glob.glob(os.path.join(self.f_in, "df3d", "pose_corr*"))[0], allow_pickle=True)
+
+		for i in range(0, 7):
+			for j in self.pose_corr[i]:
+				self.pose_corr[i][j] = self.reslice_pose_corr_to_shape(self.pose_corr[i][j], i)
+
+		self.correct_preds_stripe()
+
+	def get_image(self):
+		try:
+			return QImage(self.camera_and_frame_to_fname(self.camera, self.frame))
+		except:
+			pdb.set_trace()
+			print("I/O error, likely frame number is too high", file=sys.stderr)
+			return None
+
+	def get_data(self, belief_prop=False):
+		if belief_prop:
+			try:
+				return self.pose_corr[self.camera][self.frame]
+			except KeyError:
+				pass
+		assert self.camera in [0,1,2,4,5,6]
+		if self.camera < 3:
+			return self.preds[self.camera, self.frame, :, :]
+		elif self.camera > 3:
+			return self.preds[self.camera, self.frame, :, :]
+
+	def next_frame(self):
+		self.frame += 1
+
+	def prev_frame(self):
+		self.frame -= 1
+
+	def write(self, d):
+		"""	Writes the current frame's data to self.f_out
+			Order in the data array is different from the order in the csv file
+
+			d: array of data for this frame
+		"""
+		fpath = self.camera_and_frame_to_fname(self.camera, self.frame)
+		data_string = np_array_to_csv_string(d)
+		print(fpath+','+data_string, file=self.f_out)
+
+	def set_frame(self, frame):
+		"""	Sets the frame to 'frame'
+			returns True if successful, false otherwise
+		"""
+		self.frame = frame
+		return True
+
+	def set_camera(self, camera):
+		"""	Sets the camera to 'camera'
+			returns True if successful, false otherwise
+		"""
+		self.camera = camera
+		return True
+
+	def correct_preds_stripe(self):
+		self.preds[2, :, 16:19, :] = 0
+		self.preds[4, :, 16:19, :] = 0
+
+	def reslice_pose_corr_to_shape(self, pose_corr_slice, camera):
+		assert camera in [0,1,2,4,5,6]
+		pose_corr_slice[:,0] *= 960
+		pose_corr_slice[:,1] *= 480
+		if camera < 3:
+			return pose_corr_slice[:19, :]
+		elif camera > 3:
+			return pose_corr_slice[19:, :]
+
+	def camera_and_frame_to_fname(self, c, f):
+		return os.path.join(self.f_in, "camera_%d_img_%06d.jpg"%(c,f))
+
+class CSVDataloader(Dataloader):
+	data = []
+	f_in = ""
+	camera = 0 # camera has no meaning in this dataloader
+	frame = 0 # in this dataloader, frame has a different meaning: it refers to the line of the csv file being parsed
+
+	def __init__(self, f_in, f_out):
+		'''	f_in: path to csv file containing annotations
+			f_out: path to 
+		'''
+		self.f_in = f_in
+		self.f_out = f_out
+		self.raw_csv = []
+		with open(self.f_in) as csv_file:
+			for line in csv_file:
+				self.raw_csv.append(line.strip().split(','))
+		
+
+	def get_image(self):
+		csv_entry = self.raw_csv[self.frame]
+		self.camera = self.duel_path_to_cam_num(self.raw_csv[self.frame][0])
+		#print("camera:%d"%(self.camera))
+		#print("fpath:%s"%(self.raw_csv[self.frame][0]))
+		return QImage(csv_entry[0])
+
+	def duel_path_to_cam_num(self, path):
+		'''	Works for folder1/folder2/folder3/camera_n_img_xxxxxx.jpg
+			and folder1/folder2__folder3__folder4__camera_n_img_xxxxxx.jpg
+		'''
+		basename = os.path.basename(path)
+		img_part = basename.split("__")[-1]
+		return int(img_part[7])
+
+	def get_data(self, belief_prop=False):
+		csv_entry = self.raw_csv[self.frame]
+		self.camera = self.duel_path_to_cam_num(self.raw_csv[self.frame][0])
+		return self.csv_to_np_array(csv_entry)
+
+	def next_frame(self):
+		self.frame += 1
+
+	def prev_frame(self):
+		self.frame -= 1
+
+	def write(self, d):
+		"""	Writes the current frame's data to self.f_out
+		"""
+		print(self.raw_csv[self.frame][0] + ',' + np_array_to_csv_string(d), file=self.f_out)
+
+	def set_frame(self, frame):
+		"""	Sets the frame to 'frame'
+			returns True if successful, false otherwise
+		"""
+		self.frame = frame
+		return True
+
+	def set_camera(self, camera):
+		"""	Sets the camera to 'camera'
+			returns True if successful, false otherwise
+		"""
+		self.frame = frame
+		return True
+
+	def csv_to_np_array(self, line):
+		"""	line: data from the csv file split on commas eg. ['fpath',0,0,1,2,3,4,5,...]
+		"""
+		ANTENNA = 1
+		BACK = ANTENNA + 2*1
+		FRONT_LEG = BACK + 2*3
+		MID_LEG = FRONT_LEG + 2*5
+		BACK_LEG = MID_LEG + 2*5
+		out = np.zeros([19,2])
+
+		out[15,:] = [float(line[ANTENNA]), float(line[ANTENNA + 1])]
+
+		out[0 ,:] = [float(line[FRONT_LEG]), float(line[FRONT_LEG + 1])]
+		out[1 ,:] = [float(line[FRONT_LEG + 2*1]), float(line[FRONT_LEG + 2*1 + 1])]
+		out[2 ,:] = [float(line[FRONT_LEG + 2*2]), float(line[FRONT_LEG + 2*2 + 1])]
+		out[3 ,:] = [float(line[FRONT_LEG + 2*3]), float(line[FRONT_LEG + 2*3 + 1])]
+		out[4 ,:] = [float(line[FRONT_LEG + 2*4]), float(line[FRONT_LEG + 2*4 + 1])]
+
+		out[5 ,:] = [float(line[MID_LEG]), float(line[MID_LEG + 1])]
+		out[6 ,:] = [float(line[MID_LEG + 2*1]), float(line[MID_LEG + 2*1 + 1])]
+		out[7 ,:] = [float(line[MID_LEG + 2*2]), float(line[MID_LEG + 2*2 + 1])]
+		out[8 ,:] = [float(line[MID_LEG + 2*3]), float(line[MID_LEG + 2*3 + 1])]
+		out[9 ,:] = [float(line[MID_LEG + 2*4]), float(line[MID_LEG + 2*4 + 1])]
+
+		out[10,:] = [float(line[BACK_LEG]), float(line[BACK_LEG + 1])]
+		out[11,:] = [float(line[BACK_LEG + 2*1]), float(line[BACK_LEG + 2*1 + 1])]
+		out[12,:] = [float(line[BACK_LEG + 2*2]), float(line[BACK_LEG + 2*2 + 1])]
+		out[13,:] = [float(line[BACK_LEG + 2*3]), float(line[BACK_LEG + 2*3 + 1])]
+		out[14,:] = [float(line[BACK_LEG + 2*4]), float(line[BACK_LEG + 2*4 + 1])]
+
+		out[16,:] = [float(line[BACK]), float(line[BACK + 1])]
+		out[17,:] = [float(line[BACK + 2*1]), float(line[BACK + 2*1 + 1])]
+		out[18,:] = [float(line[BACK + 2*2]), float(line[BACK + 2*2 + 1])]
+
+
+		#out[out < 10] = 0
+		return out
+
 
 class Annotator(QWidget):
 
@@ -38,30 +284,16 @@ class Annotator(QWidget):
 	joints_list = ['body_coxa', 'coxa_femur', 'femur_tibia', 'tibia_tarsus', 'tarsus_tip']
 	limbs_list = ['coxa', 'femur', 'tibia', 'tarsus']
 
-	def __init__(self, folder, fout):
+	def __init__(self, folder, fout, dataloader):
 		super().__init__()
-		self.folder = folder
-		self.fout = fout
-		self.frame = 0
-		self.camera = 0
-		self.points_visibility = True
 
-		#self.preds = np.load(glob.glob(os.path.join(self.folder, "df3d", "pose_result*"))[0], allow_pickle=True)['points2d']
-		self.preds = np.load(glob.glob(os.path.join(self.folder, "df3d", "preds*"))[0], allow_pickle=True)
-		self.preds[:,:,:,0] *= 960
-		self.preds[:,:,:,1] *= 480
-		self.pose_corr = np.load(glob.glob(os.path.join(self.folder, "df3d", "pose_corr*"))[0], allow_pickle=True)
-
-		for i in range(0, 7):
-			for j in self.pose_corr[i]:
-				self.pose_corr[i][j] = self.reslice_pose_corr_to_shape(self.pose_corr[i][j], i)
-
-		self.correct_preds_stripe()
+		self.dataloader = dataloader
 
 		self.setGeometry(50, 50, 960, 600)
 		self.bpcheckbox = self.set_bp_checkbox()
 
-		self.this_frame_data = self.get_this_frame_pose_corr_or_pred()
+		self.points_visibility = True
+		self.this_frame_data = self.dataloader.get_data(belief_prop=self.bpcheckbox.isChecked())
 
 		self.set_radio_buttons()
 		self.set_image()
@@ -92,25 +324,12 @@ class Annotator(QWidget):
 
 		return False
 
-	def correct_preds_stripe(self):
-		self.preds[2, :, 16:19, :] = 0
-		self.preds[4, :, 16:19, :] = 0
-
-	def reslice_pose_corr_to_shape(self, pose_corr_slice, camera):
-		assert camera in [0,1,2,4,5,6]
-		pose_corr_slice[:,0] *= 960
-		pose_corr_slice[:,1] *= 480
-		if camera < 3:
-			return pose_corr_slice[:19, :]
-		elif camera > 3:
-			return pose_corr_slice[19:, :]
-
 	def joint_number_to_colour(self, jid):
 		#leg_colours = dict(front_right="#FF0000", mid_right="#0000FF", back_right="#00FF00", front_left="#FFFF00", mid_left="#FF00FF", back_left="#00FFFF")
 		if jid >= 16 and jid <= 18:
 			return "#554400" #TODO find correct colour for stripes
 
-		if self.camera < 3:
+		if self.dataloader.camera < 3:
 			if jid >= 0 and jid <= 4:
 				return "#FF0000"
 			elif jid >= 5 and jid <= 9:
@@ -119,7 +338,7 @@ class Annotator(QWidget):
 				return "#00FF00"
 			elif jid == 15:
 				return "#222222" #TODO find correct colour for antenna
-		elif self.camera > 3:
+		elif self.dataloader.camera > 3:
 			if jid >= 0 and jid <= 4:
 				return "#FFFF00"
 			elif jid >= 5 and jid <= 9:
@@ -153,14 +372,15 @@ class Annotator(QWidget):
 	def radio_button_clicked(self):
 		button = self.sender()
 		if button.isChecked():
-			self.camera = button.camera
+			self.dataloader.camera = button.camera
 		self.set_image()
 		self.update()
 
 	def frame_select_action(self):
 		box = self.sender()
 		try:
-			self.frame = int(box.text())
+			if not self.dataloader.set_frame(int(box.text())):
+				print("cannot set frame to %s"%(box.text()), file=sys.stderr)
 		except:
 			print("cannot convert %s to int"%(box.text()), file=sys.stderr)
 		box.clearFocus()
@@ -168,28 +388,11 @@ class Annotator(QWidget):
 		self.update()
 		
 	def get_this_frame_pose_corr_or_pred(self):
-		if self.bpcheckbox.isChecked():
-			try:
-				return self.pose_corr[self.camera][self.frame]
-			except KeyError:
-				pass
-		assert self.camera in [0,1,2,4,5,6]
-		if self.camera < 3:
-			#return self.preds[self.camera, self.frame, :19, :]
-			return self.preds[self.camera, self.frame, :, :]
-		elif self.camera > 3:
-			return self.preds[self.camera, self.frame, :, :]
-			#return self.preds[self.camera, self.frame, 19:, :]
+		return self.dataloader.get_data(belief_prop=self.bpcheckbox.isChecked())
 
 	def set_image(self):
-		try:
-			self.image = QImage(self.camera_and_frame_to_fname(self.camera, self.frame))
-		except:
-			print("I/O error, likely frame number is too high", file=sys.stderr)
-		self.this_frame_data = self.get_this_frame_pose_corr_or_pred()
-
-	def camera_and_frame_to_fname(self, c, f):
-		return os.path.join(self.folder, "camera_%d_img_%06d.jpg"%(c,f))
+		self.image = self.dataloader.get_image()
+		self.this_frame_data = self.dataloader.get_data(belief_prop=self.bpcheckbox.isChecked())
 
 	def draw_points(self, painter):
 		for jid, point in enumerate(self.this_frame_data, start=0):
@@ -202,40 +405,19 @@ class Annotator(QWidget):
 				painter.drawLine(round(point[0]), round(point[1]), round(self.this_frame_data[jid+1][0]), round(self.this_frame_data[jid+1][1]))
 
 	def next_frame(self):
-		self.frame += 1
+		self.dataloader.next_frame()
 		self.set_image()
-		self.frame_select.setText(str(self.frame))
+		self.frame_select.setText(str(self.dataloader.frame))
 		self.update()
 
 	def previous_frame(self):
-		self.frame -= 1
+		self.dataloader.prev_frame()
 		self.set_image()
-		self.frame_select.setText(str(self.frame))
+		self.frame_select.setText(str(self.dataloader.frame))
 		self.update()
 
 	def write_frame(self):
-		'''	Order in the self.this_frame_data array is different from the order in the csv file
-		'''
-		def make_string(d, start, stop):
-			pts = []
-			for i in range(start, stop):
-				pts.append("%03d,%03d"%(d[i,0],d[i,1]))
-			return ','.join(pts)
-
-		d = self.this_frame_data
-		fpath = self.camera_and_frame_to_fname(self.camera, self.frame)
-		antenna = make_string(d, 15, 16)
-		if self.camera in [0,1,5,6]:
-			stripe = make_string(d, 16, 19)
-		else:
-			stripe = "000,000,000,000,000,000"
-		front_leg = make_string(d, 0, 5)
-		mid_leg = make_string(d, 5, 10)
-		rear_leg = make_string(d, 10, 15)
-
-		full_string = ','.join([fpath, antenna, stripe, front_leg, mid_leg, rear_leg])
-		print(full_string, file=self.fout)
-
+		self.dataloader.write(self.this_frame_data)
 
 	def paintEvent(self, e):
 		painter = QPainter()
@@ -335,7 +517,15 @@ if __name__ == "__main__":
 	except:
 		pass
 
-	x = Annotator(sys.argv[1], fout)
+	if sys.argv[1].endswith(".csv"):
+		dataloader = CSVDataloader(sys.argv[1], fout)
+	elif os.path.isdir(sys.argv[1]):
+		dataloader = DF3DFolderDataloader(sys.argv[1], fout)
+	else:
+		print("Requires either a csv file or a df3d directory as input", file=sys.stderr)
+		exit(1)
+
+	x = Annotator(sys.argv[1], fout, dataloader)
 	app.exec_()
 
 
